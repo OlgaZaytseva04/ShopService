3749a275167fa6abeb941b77ff8152fd5b3d554e
OlgaZaytseva04 <olga.mz@mail.ru>
2021-11-14 16:02:01 +0300

    Adding empty spring boot web application

---
+++ src/main/java/olga/zaytseva/shopService/ShopServiceApplication.java
@@ -0,0 +1,13 @@
+package olga.zaytseva.shopService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/olga/zaytseva/shopService/ShopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package olga.zaytseva.shopService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
